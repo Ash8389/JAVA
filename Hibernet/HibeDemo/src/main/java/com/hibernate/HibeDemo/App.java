1d421c3efa47bf43191c06c6b0c9d592a5831c26
Ashish Jha <jhaashish270@gmail.com>
2025-12-09 07:22:10 +0530

    Hibernet Connection establish. --commit_6

---
+++ Hibernet/HibeDemo/src/main/java/com/hibernate/HibeDemo/App.java
@@ -0,0 +1,30 @@
+package com.hibernate.HibeDemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Student stud = new Student();
+        
+        stud.setRoll(1);
+        stud.setName("Rose");
+        stud.setMarks(100);
+        
+        Configuration cfg = new Configuration().configure().addAnnotatedClass(Student.class);
+        
+        SessionFactory sf = cfg.buildSessionFactory();
+        
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        
+        session.persist(stud);
+        
+        tx.commit();
+    }
+}
