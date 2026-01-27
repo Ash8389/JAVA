621e1843b25358d8b23178cd513dba597d2893d0
Ashish Jha <jhaashish270@gmail.com>
2026-01-27 14:49:14 +0530

    changed

--- 01_Basics/a8_loops.java
+++ 01_Basics/a8_loops.java
@@ -1,20 +1,20 @@
 public class a8_loops {
     public static void main(String[] args) {
         int i = 0;
-        while(i<=5){
+        while (i <= 5) {
             System.out.println("WHILE LOOP!!!");
             i++;
         }
-        
+
         do {
             System.out.println("DO WHILE LOOP!!!");
             i++;
-        } while (i<=10);
-        
-        for(int j = 1 ;j <= 5; j++){
+        } while (i <= 10);
+
+        for (int j = 1; j <= 5; j++) {
             System.out.println("FOR LOOP!!!");
         }
 
-        System.out.println("Program runs.");
+        System.out.println("Program runs successfully.");
     }
 }
